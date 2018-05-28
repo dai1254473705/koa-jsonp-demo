9b2150cc9c42c13970eeda6a7aa26c3a646f57a6
代云舟 <1254473705@qq.com>
2018-05-28 10:31:58 +0800

    提交到功能分支

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
+// require("babel-register");
+const http = require('http');
+const https = require('https');
 const Koa = require('koa');
+const nexttest = require("./nexttest");
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
 });
 
-app.listen(3000);
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+//error免密授权
+app.on('error', (err, ctx) => {
+  console.log(`server error - ${err}`);
+});
+
+// response
+app.use(async ctx => {
+  //重定向
+  // ctx.redirect('/login');
+  //异常
+  // ctx.throw(500);
+  ctx.cookies.set("name", "koa : value")
+  console.log(ctx.cookies.get("UM_distinctid"));
+  console.log(`${ctx.ip}-${ctx.cookies.get("name")}`);
+  ctx.body = ctx.response.status;
+});
+
+http.createServer(app.callback()).listen(3000);
+// https.createServer(app.callback()).listen(3000);
